61c04c5d752b69cf39141fe7dc49269354869b22
Sergey Mikhno <sergey.mikhno@gmail.com>
2019-07-07 16:18:05 +0200

    more tests

--- test_avltree.py
+++ test_avltree.py
@@ -9,6 +9,12 @@ def test_sizeafteradd1000():
         t.add(i)
     assert t.size() == 1000
 
+def test_toolist():
+    t = avltree.Avltree()
+    for i in range(100):
+        t.add(i)
+    assert t.tolist() == list(range(100))
+
 def test_afteraddandremove():
     t = avltree.Avltree()
     for i in range(1000):
@@ -17,6 +23,22 @@ def test_afteraddandremove():
         t.remove(i)
 
     assert t.size() == 0
+
+
+def test_contains():
+	t = avltree.Avltree()
+	t.add(5)
+	t.add(7)
+	t.add(9)
+	assert t.contains(5)
+	assert t.contains(7)
+	assert t.contains(9)
+	assert not t.contains(0)
+	assert not t.contains(12)
+	assert not t.contains(78)
+	assert t.size() == 3
+
+
 
 
 
