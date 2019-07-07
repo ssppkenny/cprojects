fd6462d19bcf528586bc30bde69eb93a7a496adc
Sergey Mikhno <sergey.mikhno@gmail.com>
2019-07-07 16:49:18 +0200

    more tests

--- test_avltree.py
+++ test_avltree.py
@@ -1,28 +1,33 @@
 import avltree
 import pytest
 
+def test_emptytolist():
+	t = avltree.Avltree()
+	assert t.tolist() == []
 
 def test_sizeafteradd1000():
-    t = avltree.Avltree()
+	t = avltree.Avltree()
 
-    for i in range(1000):
-        t.add(i)
-    assert t.size() == 1000
+	for i in range(1000):
+		t.add(i)
+	assert t.size() == 1000
 
 def test_toolist():
-    t = avltree.Avltree()
-    for i in range(100):
-        t.add(i)
-    assert t.tolist() == list(range(100))
+	t = avltree.Avltree()
+	for i in range(100):
+		t.add(i)
+	assert t.tolist() == list(range(100))
 
 def test_afteraddandremove():
-    t = avltree.Avltree()
-    for i in range(1000):
-        t.add(i)
-    for i in range(1000):
-        t.remove(i)
+	t = avltree.Avltree()
 
-    assert t.size() == 0
+	for j in range(1000):
+		for i in range(1000):
+			t.add(i)
+		for i in range(1000):
+			t.remove(i)
+
+	assert t.size() == 0
 
 
 def test_contains():
